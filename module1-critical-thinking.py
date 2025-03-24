120a728bb3e67640a21c7532153b89dda8981fab
Anand Verma <anand.verma@anandve-ltmnxwj.internal.salesforce.com>
2025-03-23 22:49:31 -0700

    First commit of the module 1 assignment

---
+++ module1-critical-thinking.py
@@ -0,0 +1,16 @@
+# Part 1: Addition and Substraction
+print("This program calculates the sum and difference of two integers n1 and n2 as n1+n1, and n1-n2")
+n1 = int(input("Enter the first number n1: "))
+n2 = int(input("Enter the second number n2: "))
+
+print ("Sum of n1 and n2 =", n1 + n2)
+print ("Difference of n1 and n2 =", n1 - n2)
+print()
+
+# Part 2: Multiplication and Division
+print("This program calculates the multiplication and division of two integers n1 and n2 as n1*n1, and n1/n2")
+n1 = int(input("Enter the first number n1: "))
+n2 = int(input("Enter the second number n2: "))
+
+print ("Multiplication of n1 and n2 =", n1 * n2)
+print ("Division of n1 and n2 =", n1 / n2)
