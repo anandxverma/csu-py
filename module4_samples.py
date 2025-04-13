ab9316e6d17f772245ea46977b11ab25f015206a
anandxverma <anand.x.verma@gmail.com>
2025-04-12 23:58:47 -0700

    Added Module 4 Code

--- module4_samples.py
+++ module4_samples.py
@@ -24,3 +24,27 @@ print()
 clist = ['Canada','USA','Mexico','Australia']
 for i in clist:
     print(i)
+
+list1 = ["A", "B", "C"]
+print("C" in list1)
+print("D" in list1)
+
+print()
+stu_grades = [ 3.7, 3.3, 2.7, 1.9, 3.8, 4.0, 4.0, 3.9, 3.6, 2.9, 2.1, 2.3, 2.2, 4.0, 3.8, 3.3, 3.1, 3.1, 3.3, 2.4, 2.7, 2.3, 1.9, 1.7, 3.6, 3.9, 4.0, 4.0 ]
+
+grp_high = []
+grp_med = []
+grp_low = []
+
+for i in range(len(stu_grades)):
+    grade = stu_grades[i]
+    if grade >= 3.5:
+        grp_high.append(grade)
+    elif grade < 3.5 and grade >= 2.0:
+        grp_med.append(grade)
+    else:
+        grp_low.append(grade)
+
+print(grp_high)
+print(grp_med)
+print(grp_low)
