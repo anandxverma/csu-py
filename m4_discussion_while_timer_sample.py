65db4375ebdec59073731cdaa4222b9fef92ddc2
anandxverma <anand.x.verma@gmail.com>
2025-04-14 00:58:40 -0700

    Renamed file and added code comments

--- m4_discussion_while_timer_sample.py
+++ m4_discussion_while_timer_sample.py
@@ -1,19 +1,22 @@
 import time
 
-__ACCESS_CODE = "Welcome@1"
-__MAX_RETRIES = 3
-__SLEEP_TIME = 10  # seconds
+# Constants
+__ACCESS_CODE = "Welcome@1" # The access code to be guessed
+__MAX_RETRIES = 3 # Maximum number of attempts before lockout
+__SLEEP_TIME = 10 # Time (in seconds) to wait before allowing another attempt
 
 failed_attempts = 0
 while True:
     access_code = input("Enter your access code: ")
+    # Allow access if the access code macthes
+    # Else increment the failed attempts
     if access_code == __ACCESS_CODE:
         print("Access granted.")
         break
     else:
         failed_attempts += 1
         print(f"Incorrect password. You have {__MAX_RETRIES - failed_attempts} attempts left.")
-        end_time = time.time()
+    # If the maximum number of attempts has reached, wait for a while before allowing another attempt
     if failed_attempts == __MAX_RETRIES:
         print("Access denied. Too many failed attempts. Try after " + str(int(__SLEEP_TIME)) + " seconds.")
         time.sleep(__SLEEP_TIME)
